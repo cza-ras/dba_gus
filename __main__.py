cd8709e6c1a992ee651e3c0329ecb729a1d43b7b
cezary <cezary@datafreak.pl>
2024-07-12 16:19:50 +0200

    def pobierz dane

--- __main__.py
+++ __main__.py
@@ -2,3 +2,4 @@ from dziedzinowe_bazy_wiedzy import gus_dziedzinowe_bazy
 
 if __name__ == '__main__':
     gus_dziedzinowe_bazy.pobierz_zmienne()
+    gus_dziedzinowe_bazy.pobierz_dane('310','484','2024','270')
--- dziedzinowe_bazy_wiedzy.py
+++ dziedzinowe_bazy_wiedzy.py
@@ -14,4 +14,16 @@ class gus_dziedzinowe_bazy:
         else:
             print('Połączenie z API nieudane')
 
-        wyniki.to_csv('../Projects/dbw_gus/dane/zmienne.csv', index=False)
+        wyniki.to_csv('zmienne.csv', index=False)
+
+    #pobierz dane dla zmiennj i zapisz w pliku CSV
+    def pobierz_dane(zmienna, przekroj, rok, okres):
+        api_url = f"https://api-dbw.stat.gov.pl/api/1.1.0/variable/variable-data-section?id-zmienna={zmienna}&id-przekroj={przekroj}&id-rok={rok}&id-okres={okres}&ile-na-stronie=5000"
+        r = requests.get(api_url)
+        if r.status_code == 200:
+            zmienne = json.loads(r.content)
+            wyniki = pd.DataFrame(zmienne['data'])    
+        else:
+            print('Połączenie z API nieudane')
+
+        wyniki.to_csv('dane.csv', index=False)
